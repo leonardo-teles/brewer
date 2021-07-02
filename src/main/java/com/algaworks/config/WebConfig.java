bff892a48f4e68b7174c4a8e0e7e62b4e506fb28
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-07-02 00:52:53 -0300

    Melhorando a Organização do Layout

--- src/main/java/com/algaworks/config/WebConfig.java
+++ src/main/java/com/algaworks/config/WebConfig.java
@@ -1,9 +1,12 @@
 package com.algaworks.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import nz.net.ultraq.thymeleaf.LayoutDialect;
 
 @EnableWebMvc
 @Configuration
@@ -13,5 +16,10 @@ public class WebConfig extends WebMvcConfigurerAdapter{
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+	}
+	
+	@Bean
+	public LayoutDialect layoutDialect() {
+	  return new LayoutDialect();
 	}
 }
