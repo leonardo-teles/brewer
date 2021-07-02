b1bec1dc449abcddb64eaffa9f8dbfed4a56fa59
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-07-02 19:15:45 -0300

    Prototipando o Cadastro Rápido de Estilo

--- src/main/java/com/algaworks/config/WebConfig.java
+++ src/main/java/com/algaworks/config/WebConfig.java
@@ -2,22 +2,13 @@ package com.algaworks.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import nz.net.ultraq.thymeleaf.LayoutDialect;
 
-@EnableWebMvc
 @Configuration
-@SuppressWarnings("deprecation")
-public class WebConfig extends WebMvcConfigurerAdapter{
+public class WebConfig implements WebMvcConfigurer {
 
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
-	}
-	
 	@Bean
 	public LayoutDialect layoutDialect() {
 	  return new LayoutDialect();
