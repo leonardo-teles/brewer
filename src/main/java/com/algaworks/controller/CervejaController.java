e81bcef7dc3d26464992d71211017db44420d081
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-07-05 23:47:16 -0300

    Montando os Combos e Radio do Cadastro da Cerveja

--- src/main/java/com/algaworks/controller/CervejaController.java
+++ src/main/java/com/algaworks/controller/CervejaController.java
@@ -8,33 +8,39 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.algaworks.enums.Origem;
+import com.algaworks.enums.Sabor;
 import com.algaworks.model.Cerveja;
-import com.algaworks.repository.Cervejas;
+import com.algaworks.repository.Estilos;
 
 @Controller
 @RequestMapping("/cervejas")
 public class CervejaController {
 	
 	@Autowired
-	private Cervejas cervejas;
+	private Estilos estilos;
 	
 	@GetMapping("/novo")
-	public String novo(Cerveja cerveja) {
-		cervejas.findAll();
+	public ModelAndView novo(Cerveja cerveja) {
+		ModelAndView mv = new ModelAndView("cerveja/cadastro");
+		mv.addObject("sabores", Sabor.values());
+		mv.addObject("estilos", estilos.findAll());
+		mv.addObject("origens", Origem.values());
 		
-		return "cerveja/cadastro";
+		return mv;
 	}
 	
 	@PostMapping("/novo")
-	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, RedirectAttributes attributes) {
+	public ModelAndView cadastrar(@Valid Cerveja cerveja, BindingResult result, RedirectAttributes attributes) {
 		if(result.hasErrors()) {
 			return novo(cerveja);
 		}
 		
 		attributes.addFlashAttribute("mensagem", "Cerveja salva com sucesso!");
 		
-		return "redirect:/cervejas/novo";
+		return new ModelAndView("redirect:/cervejas/novo");
 	}
 }
