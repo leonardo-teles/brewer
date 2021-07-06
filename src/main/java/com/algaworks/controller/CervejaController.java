0508351ed3fd463b67b35d3c7fd7982573f46ee6
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-07-05 22:56:00 -0300

    Configurando o Projeto para Uso do Spring Data JPA

--- src/main/java/com/algaworks/controller/CervejaController.java
+++ src/main/java/com/algaworks/controller/CervejaController.java
@@ -2,6 +2,7 @@ package com.algaworks.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,13 +11,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.model.Cerveja;
+import com.algaworks.repository.Cervejas;
 
 @Controller
 @RequestMapping("/cervejas")
 public class CervejaController {
 	
+	@Autowired
+	private Cervejas cervejas;
+	
 	@GetMapping("/novo")
 	public String novo(Cerveja cerveja) {
+		cervejas.findAll();
+		
 		return "cerveja/cadastro";
 	}
 	
---
+++ src/main/resources/db/migration/V02__criar_coluna_quantidade_estoque_em_cervejas.sql
@@ -0,0 +1,1 @@
+ALTER TABLE cervejas ADD quantidade_estoque INTEGER;
