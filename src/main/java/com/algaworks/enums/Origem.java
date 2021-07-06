2edaf16abb660d5cfb81a31eba0753ec9d13fde3
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-07-05 21:24:42 -0300

    Mapeamento da Entidade Cerveja

---
+++ src/main/java/com/algaworks/enums/Origem.java
@@ -0,0 +1,17 @@
+package com.algaworks.enums;
+
+public enum Origem {
+
+	NACIONAL("Nacional"), 
+	INTERNACIONAL("Internacional");
+
+	private String descricao;
+	
+	Origem(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+}
