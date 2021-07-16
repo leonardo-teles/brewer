e0a3a3a1d5494db3ee5a5e70eeeb78cfaa391a02
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-07-15 22:37:26 -0300

    Novo Atributo para Classe de Erro

---
+++ src/main/java/com/algaworks/thymeleaf/BrewerDialect.java
@@ -0,0 +1,27 @@
+package com.algaworks.thymeleaf;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.stereotype.Component;
+import org.thymeleaf.dialect.AbstractProcessorDialect;
+import org.thymeleaf.processor.IProcessor;
+import org.thymeleaf.standard.StandardDialect;
+
+import com.algaworks.thymeleaf.processor.ClassForErrorAttributeTagProcessor;
+
+@Component
+public class BrewerDialect extends AbstractProcessorDialect {
+
+	protected BrewerDialect() {
+		super("Algaworks Brewer", "brewer", StandardDialect.PROCESSOR_PRECEDENCE);
+	}
+
+	@Override
+	public Set<IProcessor> getProcessors(String dialectPrefix) {
+		final Set<IProcessor> processadores = new HashSet<>();
+		processadores.add(new ClassForErrorAttributeTagProcessor(dialectPrefix));
+		
+		return processadores;
+	}
+}
