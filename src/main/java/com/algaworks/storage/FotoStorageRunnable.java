f09f74fd5cae696a864fb55dc66b9febe2a03b7e
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-07-16 22:32:12 -0300

    Melhorando a Disponibilidade da Aplicação - Retorno Assíncrono

---
+++ src/main/java/com/algaworks/storage/FotoStorageRunnable.java
@@ -0,0 +1,24 @@
+package com.algaworks.storage;
+
+import org.springframework.web.context.request.async.DeferredResult;
+import org.springframework.web.multipart.MultipartFile;
+
+public class FotoStorageRunnable implements Runnable {
+	
+	private MultipartFile[] files; 
+	private DeferredResult<String> resultado;
+
+
+	public FotoStorageRunnable(MultipartFile[] files, DeferredResult<String> resultado) {
+		this.files = files;
+		this.resultado = resultado;
+	}
+
+	@Override
+	public void run() {
+		System.out.println(">>>>> files: " + files[0].getSize());
+		
+		// TODO: salvar a foto no sistema de arquivos...
+		resultado.setResult("OK! Foto recebida!");
+	}
+}
