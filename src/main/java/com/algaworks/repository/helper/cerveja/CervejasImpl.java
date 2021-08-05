d90500ca078a84b256203acb8a868e2e537f4ea5
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-08-05 17:50:29 -0300

    Ordenando a Pesquisa por SKU ou Nome

--- src/main/java/com/algaworks/repository/helper/cerveja/CervejasImpl.java
+++ src/main/java/com/algaworks/repository/helper/cerveja/CervejasImpl.java
@@ -39,7 +39,7 @@ public class CervejasImpl implements CervejasQueries {
 		
 		Sort sort = pageable.getSort();
 
-		if(sort != null) {
+		if(sort != null && sort.isSorted()) {
 			Sort.Order order = sort.iterator().next();
 			String property = order.getProperty();
 			criteria.addOrder(order.isAscending() ? Order.asc(property) : Order.desc(property));
