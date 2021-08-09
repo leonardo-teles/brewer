ea1090009f5737bf2bc075511aaf0ebf4b7b798b
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-08-09 00:59:20 -0300

    Implementando Radio TipoPessoa

--- src/main/java/com/algaworks/config/WebConfig.java
+++ src/main/java/com/algaworks/config/WebConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.github.mxab.thymeleaf.extras.dataattribute.dialect.DataAttributeDialect;
+
 import nz.net.ultraq.thymeleaf.LayoutDialect;
 
 @Configuration
@@ -12,5 +14,10 @@ public class WebConfig implements WebMvcConfigurer {
 	@Bean
 	public LayoutDialect layoutDialect() {
 	  return new LayoutDialect();
+	}
+	
+	@Bean
+	public DataAttributeDialect dataAttributeDialect() {
+		return new DataAttributeDialect();
 	}
 }
--- src/main/java/com/algaworks/controller/ClienteController.java
+++ src/main/java/com/algaworks/controller/ClienteController.java
@@ -3,13 +3,19 @@ package com.algaworks.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.algaworks.enums.TipoPessoa;
 
 @Controller
 @RequestMapping("/clientes")
 public class ClienteController {
 
 	@GetMapping("/novo")
-	public String novo() {
-		return "cliente/cadastro";
+	public ModelAndView novo() {
+		ModelAndView mv = new ModelAndView("cliente/cadastro");
+		mv.addObject("tiposPessoa", TipoPessoa.values());
+		
+		return mv;
 	}
 }
