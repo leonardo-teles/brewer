317bfd4a318282fa1aa48476e794b6665d601f0d
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-08-08 23:42:43 -0300

    Configurando Máscara de Telefone com JQuery

--- src/main/resources/static/javascripts/brewer.js
+++ src/main/resources/static/javascripts/brewer.js
@@ -16,7 +16,34 @@ Brewer.MaskMoney = (function() {
 	
 }());
 
+Brewer.MaskPhoneNumber = (function() {
+	
+	function MaskPhoneNumber() {
+		this.inputPhoneNumber = $('.js-phone-number');
+	}
+	
+	MaskPhoneNumber.prototype.iniciar = function() {
+		var maskBehavior = function (val) {
+			  return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
+		};
+			
+		var options = {
+			clearIfNotMatch: true,	
+			onKeyPress: function(val, e, field, options) {
+			  field.mask(maskBehavior.apply({}, arguments), options);
+			}
+		};
+			
+		this.inputPhoneNumber.mask(maskBehavior, options);	
+	}
+	
+	return MaskPhoneNumber;
+}());
+
 $(function() {
 	var maskMoney = new Brewer.MaskMoney();
 	maskMoney.iniciar();	
+	
+	var maskPhoneNumber = new Brewer.MaskPhoneNumber();
+	maskPhoneNumber.iniciar();
 });
