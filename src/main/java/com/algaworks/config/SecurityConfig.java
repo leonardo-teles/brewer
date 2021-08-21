5860ba69c42be0d71bbedbcd1783a08610bea15f
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-08-21 00:32:18 -0300

    Tela de Login Customizada

--- src/main/java/com/algaworks/config/SecurityConfig.java
+++ src/main/java/com/algaworks/config/SecurityConfig.java
@@ -1,13 +1,16 @@
 package com.algaworks.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
@@ -20,13 +23,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 	
 	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web
+			.ignoring()
+				.antMatchers("/layout/**")
+				.antMatchers("/images/**");
+			
+	}
+	
+	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http
-			.csrf().disable()
 			.authorizeRequests()
 				.anyRequest().authenticated()
+				.and()
+			.formLogin()
+				.loginPage("/login").permitAll()
 			.and()
-				.formLogin();
+		.csrf().disable();
 	}
 	
 	@Bean
